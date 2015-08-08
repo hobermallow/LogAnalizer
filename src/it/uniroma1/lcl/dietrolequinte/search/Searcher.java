99979412b9ba77173e38ea07663bee1e41b5fec0
steve <stenoo93@gmail.com>
2015-08-08 16:41:04 +0200

    Added EmptyDirectoryException

--- src/it/uniroma1/lcl/dietrolequinte/search/Searcher.java
+++ src/it/uniroma1/lcl/dietrolequinte/search/Searcher.java
@@ -1,5 +1,31 @@
 package it.uniroma1.lcl.dietrolequinte.search;
+import java.util.List;
+import it.uniroma1.lcl.dietrolequinte.loader.Loadable;
+import it.uniroma1.lcl.dietrolequinte.loader.Loader;
 
 public class Searcher {
 
+	static private Searcher istanza;
+	List<Loadable> listaLoader;
+	
+	static public Searcher getIstanza(String s)
+	{
+		if(istanza==null) istanza= new Searcher(s);
+		return istanza;
+	}
+	
+	static public Searcher getIstanza()
+	{
+		return istanza;
+	}
+	
+	
+	
+	private Searcher(String s)
+	{
+		Loader main = new Loader(s);
+		listaLoader = main.getLoaders();
+		
+	}
+	
 }
