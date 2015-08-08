5c6a08c0fee4ada1b134ef476e9261fde87aa90d
onoda <and22591@gmail.com>
2015-08-08 15:30:08 +0200

    Added Loader.class constructor, takes a string as argument
    Signed-off-by: onoda <and22591@gmail.com>

--- src/it/uniroma1/lcl/dietrolequinte/loader/Loader.java
+++ src/it/uniroma1/lcl/dietrolequinte/loader/Loader.java
@@ -1,13 +1,17 @@
 package it.uniroma1.lcl.dietrolequinte.loader;
 
+import java.io.File;
+
 public class Loader {
 	
 	private String directory;
+	private File fileDirectory;
 	
 	
 	public Loader(String directory) {
 		
 		this.directory = directory;
+		fileDirectory = new File(directory);
 	}
 	
 	public String getDirectory() {
