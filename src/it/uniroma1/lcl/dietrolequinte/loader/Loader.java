05b171e260f1c31e03f32f83cf3e0171726c8ff4
onoda <and22591@gmail.com>
2015-08-08 11:58:42 +0200

    Modified Loader.class, added String field "directory"
    Signed-off-by: onoda <and22591@gmail.com>

--- src/it/uniroma1/lcl/dietrolequinte/loader/Loader.java
+++ src/it/uniroma1/lcl/dietrolequinte/loader/Loader.java
@@ -1,5 +1,16 @@
 package it.uniroma1.lcl.dietrolequinte.loader;
 
 public class Loader {
+	
+	private String directory;
+	
+	public String getDirectory() {
+		return directory;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
