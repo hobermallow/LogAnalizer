5958e1fc5cdbd5f6d6d8c9e12684ad56b6b3f983
onoda <and22591@gmail.com>
2015-08-13 10:13:11 +0200

    Modified DietroLeQuinte
    Signed-off-by: onoda <and22591@gmail.com>

--- src/it/uniroma1/lcl/dietrolequinte/cmdline/DietroLeQuinte.java
+++ src/it/uniroma1/lcl/dietrolequinte/cmdline/DietroLeQuinte.java
@@ -4,38 +4,76 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
+import it.uniroma1.lcl.dietrolequinte.Utente;
 import it.uniroma1.lcl.dietrolequinte.exception.EndProgramException;
+import it.uniroma1.lcl.dietrolequinte.search.SearchResult;
 import it.uniroma1.lcl.dietrolequinte.search.Searcher;
 
 public class DietroLeQuinte {
 	
-	private String nomeDirectory;
-	private Searcher searcher;
+	static private String nomeDirectory;
+	static private Searcher searcher;
 	
 	public DietroLeQuinte(String nomeDirectory) throws EndProgramException {
 		this.nomeDirectory = nomeDirectory;
 		searcher = Searcher.getIstanza(nomeDirectory);
 	}
 	
-	private static void cicloProgramma() throws IOException {
+	private static void cicloProgramma() throws IOException, ClassNotFoundException {
 		
 		for(;;) {
 			List<String> l = prendiInput();
-			if(l.contains("exit")) return;
+			/*
+			 * Verifica il caso di uscita dal programma
+			 */
+			if(l.get(0).equals("exit")) return;
+			analizzaInput(l);
+			
 		}
 		
 	}
 	
+	private static void analizzaInput(List<String> l) throws ClassNotFoundException {
+		if(l.get(0).contains("num")) {
+			richiestaNum(l);
+		}
+		else if(l.get(0).contains("data")) {
+			richiestaData(l);
+		}
+		else richiestaGenerica(l);
+	}
+
+	private static void richiestaGenerica(List<String> l) {
+	
+	}
+
+	private static void richiestaData(List<String> l) {
+		
+	}
+
+	private static void richiestaNum(List<String> l) throws ClassNotFoundException {
+		Collection<SearchResult> list;
+		if(l.size()==2) {
+			list = searcher.search(new Utente(l.get(1)), l.get(0).split("_")[1]);
+		}
+		else {
+			list = new ArrayList<>();
+		}
+		System.out.println(list.size());
+	}
+
 	private static List<String> prendiInput() throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		return Arrays.asList(br.readLine().split("\\s+"));
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		
 		try {
 			DietroLeQuinte dlq = new DietroLeQuinte(args[0]);
