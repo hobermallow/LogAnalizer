51ced320884bd3a2a30260ea3fd6cfb2a16acbb2
steve <stenoo93@gmail.com>
2015-08-18 17:48:48 +0200

    Added SearchResult javadoc

--- src/it/uniroma1/lcl/dietrolequinte/search/SearchResult.java
+++ src/it/uniroma1/lcl/dietrolequinte/search/SearchResult.java
@@ -2,13 +2,29 @@ package it.uniroma1.lcl.dietrolequinte.search;
 
 import it.uniroma1.lcl.dietrolequinte.AbstractInterrogazione;
 
+/**
+ * Classe che definisce un risultato  
+ * @author Steve
+ *
+ */
 public class SearchResult {
 
 	AbstractInterrogazione i;
 	
+	/**
+	 * Costruttore, un SearchResult e' costruito a partire da un oggetto AbstractInterrogazione
+	 * @param i AbstractInterrogazione
+	 */
 	public SearchResult(AbstractInterrogazione i){this.i=i;}
 	
+	/**
+	 * 
+	 * @return ritorna AbstractInterrogazione su cui e' costruito l'oggetto
+	 */
 	public AbstractInterrogazione getInterrogazione(){return i;}
 	
+	/**
+	 * rappresentazione a stringa del risultato
+	 */
 	public String toString(){return i.toString();}
 }
